5d5a69ca81b59c00563a2e94cc773af4c0e29dc6
Maxime RIO <maxime.rio@unibas.ch>
2017-12-20 16:27:06 +0100

    optimization

--- ast_logpdf.m
+++ ast_logpdf.m
@@ -3,7 +3,5 @@ function logp = ast_logpdf(alpha, mu, nu1, nu2, sigma, y)
     logp_left = ast_logpdf_left(alpha, mu, nu1, sigma, y);
     logp_right = ast_logpdf_right(alpha, mu, nu2, sigma, y);
     mask = y < mu;
-    logp = zeros(size(logp_left));
-    logp(mask) = logp_left(mask);
-    logp(~mask) = logp_right(~mask);
+    logp = mask .* logp_left + ~mask .* logp_right;
 end
--- ast_logpdf_grad.m
+++ ast_logpdf_grad.m
@@ -4,13 +4,9 @@ function [g_mu, g_sigma] = ast_logpdf_grad(alpha, mu, nu1, nu2, sigma, y)
 
     g_mu_left = ast_logpdf_left_grad_mu(alpha, mu, nu1, sigma, y);
     g_mu_right = ast_logpdf_right_grad_mu(alpha, mu, nu2, sigma, y);
-    g_mu = zeros(size(g_mu_left));
-    g_mu(mask) = g_mu_left(mask);
-    g_mu(~mask) = g_mu_right(~mask);
+    g_mu = mask .* g_mu_left + ~mask .* g_mu_right;
 
     g_sigma_left = ast_logpdf_left_grad_sigma(alpha, mu, nu1, sigma, y);
     g_sigma_right = ast_logpdf_right_grad_sigma(alpha, mu, nu2, sigma, y);
-    g_sigma = zeros(size(g_sigma_left));
-    g_sigma(mask) = g_sigma_left(mask);
-    g_sigma(~mask) = g_sigma_right(~mask);
+    g_sigma = mask .* g_sigma_left + ~mask .* g_sigma_right;
 end
