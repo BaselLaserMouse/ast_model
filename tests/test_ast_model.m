dbbf5e12a0bdb429b29103772a01ed268c356330
Maxime RIO <maxime.rio@unibas.ch>
2018-01-08 13:58:48 +0100

    minor change + added test data (real cells)

--- tests/test_ast_model.m
+++ tests/test_ast_model.m
@@ -9,9 +9,6 @@ cell_sig = abs(randn(1, nt)) * 10;
 traces = [cell_sig + npil_sig * 0.7 + 5 + randn(1, nt);
           npil_sig + 7 + randn(1, nt)];
 
-traces_std = std(traces(:));
-traces = traces / traces_std;
-
 %% try to decontaminate signal
 
-[coeff, mu, offset, sigma] = fit_ast_model(traces, [1, 1]);
+cleaned_trace = fit_ast_model(traces, [1, 1], 'verbose', 2);
